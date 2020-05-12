9c956a77db2422a95f014b12ce0bd29ff8ce9d5b
jwosborn <osbornjeremyw@yahoo.com>
2020-05-12 16:26:34 -0400

    ex_docs dep and docs

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,8 +1,15 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards 
+    """
 
+    @doc """
+    Returns a list of strings that represent playing cards
+    """
   def create_deck do 
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
     for suit <- suits, value <- values do 
         "#{value} of #{suit}" 
     end
@@ -16,6 +23,9 @@ defmodule Cards do
     Enum.member?(deck, card)
   end 
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck. The `hand_size` argument indicates how many cards should be in the hand.
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -26,11 +36,15 @@ defmodule Cards do
   end
 
   def load(filename) do 
-    {status, binary} = File.read(filename)
-
-    case status do 
-    :ok -> :erlang.binary_to_term binary
-    :error -> "That file does not exist"
+    case File.read(filename) do 
+    {:ok, binary} -> :erlang.binary_to_term(binary)
+    {:error, _reason} -> "That file does not exist"
     end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
