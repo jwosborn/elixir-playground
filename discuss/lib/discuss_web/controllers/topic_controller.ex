8f2310467d460ccf9f570b569f989af7ebdf743e
jwosborn <osbornjeremyw@yahoo.com>
2020-05-18 15:28:50 -0400

    adding new topic redirects to new /index, lists all topics

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -3,18 +3,23 @@ defmodule DiscussWeb.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"topic" => topic} = params) do
+  def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} ->
-        IO.inspect(post)
+      {:ok, _post} ->
+        redirect(conn, to: "/")
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
